7e11ade8b7f337d9aff5b340e13c69c36e452326
Tavilo Breno <breno_wk2@hotmail.com>
2025-05-28 11:28:04 -0300

    Configurando a estrutura do projeto

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,8 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-    throw new Error("This is a test error");
-});
+router.post("/users", new CreateUserController().handle);
 
 export { router };
