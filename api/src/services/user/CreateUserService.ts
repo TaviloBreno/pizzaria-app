3b13d30c21063308d5ff34ce821b305b4ce0b6a8
Tavilo Breno <breno_wk2@hotmail.com>
2025-05-28 11:54:55 -0300

    Cadastrando usuário

--- api/src/services/user/CreateUserService.ts
+++ api/src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClent from "../../prisma";
+
 interface UserRequest{
     name: string;
     email: string;
@@ -6,7 +8,34 @@ interface UserRequest{
 
 class CreateUserService{
     async execute({ name, email, password }: UserRequest){
-        
+        if(!email){
+            throw new Error("Email incorrect");
+        }
+
+        const userAlreadyExists = await prismaClent.user.findFirst({
+            where: {
+                email: email
+            }
+        });
+
+        if(userAlreadyExists){
+            throw new Error("User already exists!");
+        }
+
+        const user = await prismaClent.user.create({
+            data: {
+                name,
+                email,
+                password
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            }
+        });
+
+        return user;
     }
 }
 
