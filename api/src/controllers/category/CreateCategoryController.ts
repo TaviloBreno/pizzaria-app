491784f3cac1ff4e094969817c3c25b0f1e50871
Tavilo Breno <breno_wk2@hotmail.com>
2025-05-29 18:55:07 -0300

    Cadastro de categorias

---
+++ api/src/controllers/category/CreateCategoryController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { CreateCategoryService } from '../../services/category/createCategoryService';
+
+class CreateCategoryController {
+    async handle(request: Request, response: Response) {
+        const { name } = request.body;
+
+        const createCategoryService = new CreateCategoryService();
+
+        const category = await createCategoryService.execute({
+            name
+        });
+
+        return response.json(category);
+    } 
+}
+
+export { CreateCategoryController };
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
@@ -10,5 +11,8 @@ router.post("/users", new CreateUserController().handle);
 router.post("/login", new AuthUserController().handle);
 
 router.get("/me", isAuthenticated, new AuthUserController().handle);
+
+// Rotas de categorias
+router.post("/categories", isAuthenticated, CreateCategoryController().handle);
 
 export { router };
