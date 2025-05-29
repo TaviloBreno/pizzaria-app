b63339ca939ef44374cb0d16c5c077644feebe49
Tavilo Breno <breno_wk2@hotmail.com>
2025-05-29 19:37:12 -0300

    Listando categorias

--- api/src/middlewares/isAuthenticated.ts
+++ api/src/middlewares/isAuthenticated.ts
@@ -1,26 +1,30 @@
-import { NextFunction, Request, Response } from 'express';
-import { verify } from 'jsonwebtoken';
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
 
-interface Payload{
-    sub: string;
+interface Payload {
+  sub: string;
 }
 
-export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
-    const authToken = req.headers.authorization;
+export function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  const authToken = req.headers.authorization;
 
-    if(!authToken) {
-        return res.status(401).json({ error: "Token is missing" });
-    }
+  if (!authToken) {
+    res.status(401).json({ error: "Token is missing" });
+    return;
+  }
 
-    const [, token] = authToken.split(" ");
+  const [, token] = authToken.split(" ");
 
-    try{
-        const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
-
-        req.user_id = sub;
-
-        return next();
-    }catch(err) {
-        return res.status(401).json({ error: "Invalid token" });
-    }
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET as string) as Payload;
+    req.user_id = sub;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Invalid token" });
+    return;
+  }
 }
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,16 +3,23 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 const router = Router();
 
-router.post("/users", new CreateUserController().handle);
+const createUserController = new CreateUserController();
 
-router.post("/login", new AuthUserController().handle);
+router.post(
+  "/users",
+  new CreateUserController().handle.bind(new CreateUserController())
+);
 
-router.get("/me", isAuthenticated, new AuthUserController().handle);
+router.post("/login", new AuthUserController().handle.bind(new AuthUserController()));
+
+router.get("/me", isAuthenticated, new AuthUserController().handle.bind(new AuthUserController()));
 
 // Rotas de categorias
-router.post("/categories", isAuthenticated, CreateCategoryController().handle);
+router.post("/categories", isAuthenticated, new CreateCategoryController().handle.bind(new CreateCategoryController()));
+router.get("/categories", isAuthenticated, new ListCategoryController().handle.bind(new ListCategoryController()));
 
 export { router };
